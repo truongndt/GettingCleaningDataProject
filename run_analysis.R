35e92ae6847aeefee129595c470f2f8fb4d1f240
Truong Nguyen <ndthaotruong@gmail.com>
2016-02-28 22:32:37 +0700

    Added files via upload

---
+++ run_analysis.R
@@ -0,0 +1,57 @@
+### Get data & Unzip
+
+if (!file.exists('UCI HAR Dataset')) {
+  url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(url, destfile = "dataset.zip")
+  unzip("dataset.zip")
+}
+
+# 1. Merges the training and the test sets to create one data set.
+
+## Read train data
+
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt",header = FALSE)
+X_train <- read.table("UCI HAR Dataset/train/X_train.txt",header = FALSE)
+Y_train <- read.table("UCI HAR Dataset/train/y_train.txt",header = FALSE)
+
+## Read train data
+
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt",header = FALSE)
+X_test <- read.table("UCI HAR Dataset/test/X_test.txt",header = FALSE)
+Y_test <- read.table("UCI HAR Dataset/test/y_test.txt",header = FALSE)
+
+## Combine data
+
+X <- rbind(X_train, X_test)
+Y <- rbind(Y_train, Y_test)
+S <- rbind(subject_train, subject_test)
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+
+## Read train data
+
+features <- read.table("UCI HAR Dataset/features.txt", header = FALSE)
+mdfeatures <- grepl("mean\\(\\)|std\\(\\)",features$V2)
+
+## Extracts measurements
+
+X <- X[,mdfeatures]
+
+# 3. Uses descriptive activity names to name the activities in the data set.
+
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", header = FALSE)
+Y[, 1] <- activity_labels[Y[, 1], 2]
+
+# 4. Appropriately labels the data set with descriptive variable names.
+names(X) <- gsub("\\(|\\)", "", (features[mdfeatures, 2]))
+names(Y) <- "Activity"
+names(S) <- "Subject"
+data <- cbind(S,Y,X)
+
+#5. Create summary dataset with means for each variable by each actifity and subject.
+
+melted = melt(data, id.var = c("Subject", "Activity"))
+tidydata = dcast(melted , Subject + Activity ~ names(X), mean)
+
+# Tidy data file
+write.table(tidydata, file = "tidydata.txt", row.name = FALSE)
